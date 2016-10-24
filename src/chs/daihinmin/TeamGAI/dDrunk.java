77d90f4112ad429b2712b6cbbd67cd50857eff30
aaadmy <atuy.adamay@gmail.com>
2016-10-24 17:17:42 +0900

    update second stage

--- src/chs/daihinmin/TeamGAI/dDrunk.java
+++ src/chs/daihinmin/TeamGAI/dDrunk.java
@@ -10,36 +10,49 @@ import chs.daihinmin.TeamGAI.base.*;
 public class dDrunk extends BotSkeleton {
 	Default defaultStrategy = new Default();
 	Dashiosimi nomalStrategy = new Dashiosimi();
+	SecondStage secStage = new SecondStage();
 	PatternMake patMaker = new PatternMake();
 	PlayedCardList cardList;
+
+	boolean isGameStart = false;
 
 	/*
 	 * public Cards requestingGivingCards(){
 	 * 
-	 * return null; }
+	 * return null;
 	 */
-	public void gameStarted(){
+	public void gameStarted() {
 		super.gameStarted();
 		cardList = new PlayedCardList();
+		isGameStart = false;
 	}
-	
-	public void played(Integer num, Meld playedMeld){
+
+	public void played(Integer num, Meld playedMeld) {
 		super.played(num, playedMeld);
 		// 場に出たカードを記録
 		cardList.updateList(playedMeld);
 	}
-	
+
 	public Cards requestingGivingCards() {
 		Cards hand = this.hand();
 		Rules rules = this.rules();
 		int rank = this.rank();
-		
-		//Cards result = Default.requestingGivingCards();
+
+		// Cards result = Default.requestingGivingCards();
 		Cards result = Dashiosimi.requestingGivingCards(hand, rules, rank);
 		return result;
 	}
 
-	public Meld requestingPlay() { 
+	public Meld requestingPlay() {
+		//初回のみ手札のカードを未登場カードリストから除く
+		if(!isGameStart){
+			isGameStart = true;
+			cardList.updateList(this.hand());
+		}
+		
+		//現在のカード状況で考えられる役を登録
+		cardList.updateMeldsList();
+		
 		// 役の作成
 		//Melds melds = Melds.parseMelds(this.hand());
 		Melds melds = patMaker.patMake(this.hand());
@@ -49,11 +62,15 @@ public class dDrunk extends BotSkeleton {
 		Rules rules = this.rules();
 
 		//提出用の役
-		Meld playMeld = nomalStrategy.requestingPlay(melds, place, rules);
+		Meld playMeld = secStage.requestingPlay(melds, place, rules, cardList);
+		//Meld playMeld = nomalStrategy.requestingPlay(melds, place, rules);
 		
 		//return defaultStrategy.requestingPlay(melds, place, rules);
 		cardList.updateList(playMeld);
 		cardList.showCards();
+		
+		//自分の使ったカードを記録
+		cardList.setLastPlayedMeld(playMeld);
 		return playMeld;
 	}
 
--- src/chs/daihinmin/TeamGAI/strategy/SecondStage.java
+++ src/chs/daihinmin/TeamGAI/strategy/SecondStage.java
@@ -1,7 +1,82 @@
 package chs.daihinmin.TeamGAI.strategy;
 
+import jp.ac.uec.daihinmin.player.*;
+import jp.ac.uec.daihinmin.*;
+import jp.ac.uec.daihinmin.card.*;
+import static jp.ac.uec.daihinmin.card.MeldFactory.PASS;
+
+import chs.daihinmin.TeamGAI.base.*;
+
+import java.util.HashMap;
+
 public class SecondStage {
 	/**
 	 * 中盤の動きを表した関数
 	 */
+
+	HashMap<Meld, Double> meldHash = new HashMap<Meld, Double>();
+
+	public Meld requestingPlay(Melds melds, Place place, Rules rules, PlayedCardList pList) {
+		double nomalValue = 0;
+		// 手札の役の点数表を作る
+		meldHash.clear();
+		for (Meld m : melds) {
+			Double val = new Double( pList.calcMeldValue(m, rules, place.order()) );
+			meldHash.put(m, val);
+			nomalValue += val;
+		}
+		nomalValue /= melds.size();
+
+		// 場がからのとき
+		if (place.isRenew()) {
+			// 革命できてそのほうが強そうならやる
+			Melds canRevol = melds.extract(Melds.GROUPS.and(Melds.sizeOver(3)).or(Melds.SEQUENCES.and(Melds.sizeOver(4))));
+			if (!canRevol.isEmpty()) {
+				double reverseValue = pList.calcMeldsVps(melds, rules, place.order().reverse());
+				if (nomalValue > reverseValue) {
+					return canRevol.extract(Melds.MAX_RANK).get(0);
+				}
+			}
+			// そうでなければスペ３単騎以外の弱いのを出す
+			Melds playMelds = melds.extract(Melds.SINGLES
+					.and(Melds.rankOf(Rank.valueOf(3)))
+					.and(Melds.suitsOf(Suits.valueOf(Suit.SPADES))));
+			if(playMelds.isEmpty()) return PASS;
+			return playMelds.extract(Melds.MIN_RANK).get(0);
+		} else {
+			// 場にカードが入っているとき
+			if (place.lastMeld() == pList.lastPlayedMeld) {
+				// 最後に出したのが自分ならパス
+				return PASS;
+			}
+
+			//ここからいつもの
+			Rank next_rank;
+			try {
+				next_rank = place.type() == Meld.Type.SEQUENCE
+						? rules.nextRankSequence(place.rank(), place.size(), place.order())
+						: place.order() == Order.NORMAL ? place.rank().higher() : place.rank().lower();
+			} catch (IllegalArgumentException e) {
+				// 場に出されている役より，ランクの大きい役が存在しないとき
+				return PASS;
+			}
+
+			// 場に出されている役の,タイプ,枚数,ランク,革命中か否か,に合わせて,「出すことができる」候補に絞る
+			melds = melds.extract(Melds.typeOf(place.type()).and(Melds.sizeOf(place.size())));
+
+			melds = melds.extract(Melds.rankOf(next_rank)
+					.or(place.order() == Order.NORMAL ? Melds.rankOver(next_rank) : Melds.rankUnder(next_rank)));
+			//ここまでいつもの
+			if(melds.isEmpty()) return PASS;
+			Melds.sort(melds);
+			
+			// 弱いのが出せるなら出す
+			if(meldHash.get(melds.get(0)) > nomalValue){
+				return melds.get(0);
+			}
+			// 強いのが出せるなら出す
+			return PASS;
+		}
+		
+	}
 }
